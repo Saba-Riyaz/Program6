95fb3de12d953a7723015eee6f61faa5fe8a56ec
Saba Riaz <sabaawan665@gmail.com>
2023-07-24 11:21:06 +0500

    Commit feat : Program6 Solution

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<int> IndexOfCapitals(string inputString)
+    {
+        List<int> indices = new List<int>();
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            if (char.IsUpper(inputString[i]))
+            {
+                indices.Add(i);
+            }
+        }
+        return indices;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string inputString = Console.ReadLine();
+
+        List<int> result = IndexOfCapitals(inputString);
+
+        Console.Write("Indices of capital letters: ");
+        if (result.Count == 0)
+        {
+            Console.WriteLine("None");
+        }
+        else
+        {
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
